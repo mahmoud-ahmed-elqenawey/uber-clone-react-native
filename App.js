f0525598325e9bb6ff9aac2d3a743f52e85e5dad
QeNo <moodiiy.98@gmail.com>
2022-08-12 11:05:07 +0200

    add redux

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// redux
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+    return (
+        <Provider store={store}>
+            <View>
+                <Text>Qeno</Text>
+                {/* <StatusBar style='auto' /> */}
+            </View>
+        </Provider>
+    );
+}
---
+++ redux/slices/navSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const navSlice = createSlice({
+    name: 'navSlice',
+    initialState,
+    reducers: {},
+});
+
+export const {} = navSlice.actions;
+
+export default navSlice.reducer;
