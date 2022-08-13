4e4d52cf3c201ad3a9fd8c0d01a61c7d2c37f1d3
QeNo <moodiiy.98@gmail.com>
2022-08-13 16:11:31 +0200

    add nav slice

--- redux/slices/navSlice.js
+++ redux/slices/navSlice.js
@@ -1,13 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {};
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInfoRmation: null,
+};
 
 export const navSlice = createSlice({
-    name: 'navSlice',
+    name: 'nav',
     initialState,
-    reducers: {},
+    reducers: {
+        setOrigin: (state, action) => {
+            state.origin = action.payload;
+        },
+        setDestination: (state, action) => {
+            state.destination = action.payload;
+        },
+        setTravelTimeInfoRmation: (state, action) => {
+            state.travelTimeInfoRmation = action.payload;
+        },
+    },
 });
 
-export const {} = navSlice.actions;
+export const { setOrigin, setDestination, setTravelTimeInfoRmation } =
+    navSlice.actions;
+
+// selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInfoRmation = (state) =>
+    state.nav.travelTimeInfoRmation;
 
 export default navSlice.reducer;
