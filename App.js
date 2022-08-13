8fd7543e38e1c81d3f7dcc3a330c992999d259e8
QeNo <moodiiy.98@gmail.com>
2022-08-13 18:12:17 +0200

    add nav options and inizialize app

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 // redux
-import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import EatsScreen from './components/EatsScreen';
+// import android.os.Bundle
 
 export default function App() {
+    const Stack = createNativeStackNavigator();
     return (
         <Provider store={store}>
-            <View>
-                <Text>Qeno</Text>
-                {/* <StatusBar style='auto' /> */}
-            </View>
+            <NavigationContainer>
+                <SafeAreaProvider>
+                    <Stack.Navigator>
+                        <Stack.Screen
+                            name='HomeScreen'
+                            component={HomeScreen}
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                        <Stack.Screen
+                            name='MapScreen'
+                            component={MapScreen}
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                        <Stack.Screen
+                            name='EatsScreen'
+                            component={EatsScreen}
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                    </Stack.Navigator>
+                    {/* <HomeScreen /> */}
+                </SafeAreaProvider>
+            </NavigationContainer>
         </Provider>
     );
 }
